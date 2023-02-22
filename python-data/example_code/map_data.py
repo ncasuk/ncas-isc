8c4e7f5c43803c31ed3e9ff98c0b74e8b688992f
dwest77a <daniel.westwood@stfc.ac.uk>
2023-02-22 09:42:39 +0000

    Fixed links to example_data/example_code except for matplotlib solutions 'from'

--- python-data/example_code/map_data.py
+++ python-data/example_code/map_data.py
@@ -10,7 +10,7 @@ from netCDF4 import Dataset
 import numpy as np
 
 # Open netCDF dataset and extract data
-dataset = Dataset("example_data/tas.nc")
+dataset = Dataset("../example_data/tas.nc")
 tas = dataset.variables['tas'][0,:].squeeze()
 lons = dataset.variables['lon'][:]
 lats = dataset.variables['lat'][:]
