690dca3011bb7471117ec0ce8e9067b4398925f9
Alex Manning <alexander.manning@stfc.ac.uk>
2024-11-21 11:20:46 +0000

    Add helper script to update the content.

---
+++ python-data/update-script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+set -e
+
+# Make sure isc is up to date.
+git -C ~/ncas-isc pull
+
+# Update the files.
+cp ~/ncas-isc/python-data/exercises/ex04_cf_python.ipynb ~/my-isc-work/python-data/exercises/ex04_cf_python.ipynb
+cp -r ~/ncas-isc/python-data/data/ ~/my-isc-work/python-data/data
+cp ~/ncas-isc/python-data/solutions/ex04_cf_python.ipynb ~/my-isc-work/python-data/solutions/ex04_cf_python.ipynb
+cp -r ~/ncas-isc/python-data/notebooks ~/my-isc-work/python-data/notebooks
